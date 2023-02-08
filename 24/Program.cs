f32539f2a335a8ac8dea978ed15c09f27be26cf8
Elya Megrabyan <m-elia@yandex.ru>
2023-02-08 20:48:09 +0300

    24

---
+++ 24/Program.cs
@@ -0,0 +1,17 @@
+﻿// сумма чисел от 1 до А
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n=int.Parse(Console.ReadLine());
+
+int sum (int a) {
+    int s=0;
+    for (int i=1; i<=a; i++){
+    s +=i;
+          }
+    return s;
+}
+
+
+
+Console.Write($"Ответ {sum(n)}");
