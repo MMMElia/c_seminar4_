211e47183de659db1e71d556ec1c5b717cbe7a11
Elya Megrabyan <m-elia@yandex.ru>
2023-02-08 21:22:47 +0300

    26

---
+++ 26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает 
+// на вход число N и выдаёт количество цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n=Int32.Parse(Console.ReadLine());
+
+
+int s=num(n);
+Console.Write($"Ответ: {s}");
+
+int num(int a1) {
+    int i=0;
+    if (a1<0) {a1*=(-1);}
+    while (a1>0) {
+        i++;
+        a1/=10;
+       
+            }
+return num;
+}
