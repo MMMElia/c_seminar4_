86091e57e28a28ab1cde3662a9bf1c4162a3577c
Elya Megrabyan <m-elia@yandex.ru>
2023-02-09 00:24:43 +0300

    Домашняя задача 27

---
+++ DZ_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n=int.Parse(Console.ReadLine());
+
+int method(int a)
+{
+int i=0;
+while (a>0)
+    {
+        i=i+a%10;
+        a/=10;
+    }
+return i;
+}
+
+Console.Write($"Ответ: {method(n)}");
