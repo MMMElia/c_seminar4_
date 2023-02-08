824448e993c93076d25f5bc696f6760457a84754
Elya Megrabyan <m-elia@yandex.ru>
2023-02-08 23:08:42 +0300

    30

---
+++ 28/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает 
+//на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int s=int.Parse(Console.ReadLine());
+
+int pr(int a)
+{
+    int x=1; 
+    int i=1;
+    for (i=2; i<=a; i++)
+        {
+            x=x*i;
+           
+        }
+    return x;
+}
+
+Console.Write($"Ответ: {pr(s)}");
---
+++ 30/Program.cs
@@ -0,0 +1,22 @@
+﻿// массив из 0 и 1 рандомно
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n=int.Parse(Console.ReadLine());
+
+int[]arr=Input(n);
+
+
+
+int[] Input(int n1) 
+{
+int[] array= new int[n1];
+int i=0;
+for (i=0; i<n1; i++)
+    {
+        array[i]=new Random().Next(0,2);
+        Console.Write($"{array[i]} ");
+    }
+return array;
+}
+
